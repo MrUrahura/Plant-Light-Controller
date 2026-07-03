0e50182f1560e8e574e869fe640596301c537b5d
Alec Volodarskiy <volodarskiyalec@gmail.com>
2026-07-02 23:02:43 -0700

    Improved ServoController
    -Added the private servos to the .h file
    -Added the top servo (which was missing) to the begin
    -Got rid of the Wire.h inclusion which was unnecessary

--- lib/ServoController/ServoController.cpp
+++ lib/ServoController/ServoController.cpp
@@ -1,13 +1,13 @@
-#include <Wire.h>
-#include <ESP32Servo.h>
+#include <Arduino.h>
 #include "ServoController.h"
 #include "Pins.h"
 
-Servo topServo;
-Servo leftServo;
-Servo rightServo;
+ServoController::ServoController() {
+    
+}
 
 void ServoController::begin() {
+    topServo.attach(TOP_SERVO_PIN);
     leftServo.attach(LEFT_SERVO_PIN);
     rightServo.attach(RIGHT_SERVO_PIN);
 }
--- lib/ServoController/ServoController.h
+++ lib/ServoController/ServoController.h
@@ -1,9 +1,16 @@
 #pragma once
+#include <ESP32Servo.h>
 
 class ServoController {
 public:
+    ServoController();
     void begin();
     void setTopServoAngle(int angle);
     void setLeftServoAngle(int angle);
     void setRightServoAngle(int angle);
+
+private:
+    Servo topServo;
+    Servo leftServo;
+    Servo rightServo;
 };
