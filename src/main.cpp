9472ab184607ff9a7d7d26f8ddcf5827547c652f
Alec Volodarskiy <volodarskiyalec@gmail.com>
2026-07-02 21:32:37 -0700

    Set up the main.cpp file
    -Added all includes
    -Set up a state system
    -Need to make the bootup in setup() and fill out the switch in loop()

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,96 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include "Pins.h"
 #include "Plant.h"
 #include "LightSensor.h"
 #include "ServoController.h"
+#include "WeatherForecast.h"
+#include "TimeManager.h"
+#include "DLIController.h"
+#include "SettingsManager.h"
+#include "NetworkManager.h"
+#include "AppCommunication.h"
 
 // Components declarations
 Plant currentPlant;
 LightSensor sensor;
 ServoController blinds;
+DLITracker dliTracker;
+AppCommunication appComm;
+SettingsManager settings;
+NetworkManager network(settings);
+TimeManager timeManager(settings, network, currentPlant.getPhotoperiod());
+WeatherForecast weather(settings, network, timeManager);
 
-// Variables declarations
-float currentLux;
-float currentPPFD;
-float currentDLI;
+// Enum to represent the state of the system
+enum class SystemState {
+  BOOTING,
+  WAITING_FOR_SETUP,
+  CONNECTING_WIFI,
+  SYNCING_TIME,
+  FETCHING_WEATHER,
+  READY
+};
+SystemState currentState = SystemState::BOOTING;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
-  sensor.begin();
-  blinds.begin();
+  // Initialize components along with their "begin" or "load" method
+  settings.load();
+  network.begin();
+
+  if (settings.isFullConfigured())
+  {
+    currentState = SystemState::CONNECTING_WIFI;
+  }
+  else
+  {
+    currentState = SystemState::BLUETOOTH_SETUP;
+  }
 
   Serial.println("System initialized.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  currentLux = sensor.readLightLevel();
-  currentPPFD = currentLux / 54.0; // Convert lux to PPFD (approximation)
-  currentDLI += currentPPFD * 60.0 / 1000000.0;
+  network.handle(); // Handle network reconnections if needed
 
-  Serial.print("Lux: ");
-  Serial.println(currentLux);
-  
-  Serial.print("PPFD: ");
-  Serial.println(currentPPFD);
+  // Check the system state and perform actions accordingly
+  switch (currentState) {
+    case SystemState::BLUETOOTH_SETUP:
+      Serial.println("Waiting for Bluetooth setup...");
+      // Handled when user configures the device via Bluetooth
+      currentState = SystemState::CONNECTING_WIFI;
+      break;
 
-  Serial.print("DLI: ");
-  Serial.println(currentDLI);
+    case SystemState::CONNECTING_WIFI:
+      Serial.println("Connecting to WiFi...");
+      // Attempt to connect to wifi (maybe not necessary due to handle() in loop, but ensures connection attempt)
+      network.connectWiFi();
+      if (network.isConnected()) {
+        Serial.println("WiFi connected.");
+        currentState = SystemState::SYNCING_TIME;
+      }
+      break;
+
+    case SystemState::SYNCING_TIME:
+      Serial.println("Syncing time...");
+      // Call TimeManager's begin function to sync time
+      timeManager.begin();
+      currentState = SystemState::FETCHING_WEATHER;
+      break;
+
+    case SystemState::FETCHING_WEATHER:
+      Serial.println("Fetching weather data...");
+      // Call WeatherForecast's update function to fetch weather data
+      weather.update();
+      currentState = SystemState::READY;
+      break;
+
+    case SystemState::READY:
+      Serial.println("System is ready.");
+      // Perform regular operations like reading sensors and controlling blinds
+      break;
+  }
 
   delay(1000);
 }
