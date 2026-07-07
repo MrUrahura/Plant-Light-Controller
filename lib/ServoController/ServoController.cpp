690bbcb37f08adc8ae921728f91c56e111d087be
Alec Volodarskiy <volodarskiyalec@gmail.com>
2026-07-06 21:10:01 -0700

    Expanded ServoController
    -Made an enum flag system to represent the different shades and their states
    -Output that enum flag system to LightController to iterate through all the possible states for the algorithm and then take in the optimal state from LightController to set the shades to that

--- lib/ServoController/ServoController.cpp
+++ lib/ServoController/ServoController.cpp
@@ -7,7 +7,45 @@ ServoController::ServoController() {
 }
 
 void ServoController::begin() {
+    shadeState = static_cast<uint8_t>(ShadeID::NONE);
+}
+
+void ServoController::begin() {
     topServo.attach(TOP_SERVO_PIN);
     leftServo.attach(LEFT_SERVO_PIN);
     rightServo.attach(RIGHT_SERVO_PIN);
+}
+
+uint8_t ServoController::getCurrentState() {
+    return shadeState;
+}
+
+bool ServoController::areShadesClosed(ShadeID shades) const {
+    return (shadeState & shades) != 0;
+}
+
+void ServoController::setShades(ShadeID shades) {
+    setShades(static_cast<uint8_t>(shades));
+}
+
+void ServoController::setShades(uint8_t shades) {
+    shadeState |= shades;
+    
+    if (shadeState & ShadeID::TOP_SHADE) {
+        // close the shade
+    } else {
+        // open the shade
+    }
+
+    if (shadeState & ShadeID::LEFT_SHADE) {
+        // close the shade
+    } else {
+        // open the shade
+    }
+
+    if (shadeState & ShadeID::RIGHT_SHADE) {
+        // close the shade
+    } else {
+        // open the shade
+    }
 }
--- lib/ServoController/ServoController.h
+++ lib/ServoController/ServoController.h
@@ -5,12 +5,36 @@ class ServoController {
 public:
     ServoController();
     void begin();
-    void setTopServoAngle(int angle);
-    void setLeftServoAngle(int angle);
-    void setRightServoAngle(int angle);
+
+    // Enum to represent the state of the shades
+    enum class ShadeID : uint8_t {
+        NONE = 0,
+        TOP_SHADE = 1 << 0,
+        LEFT_SHADE = 1 << 1,
+        RIGHT_SHADE = 1 << 2,
+        ALL = 0x07
+    };
+    uint8_t getCurrentState();
+    bool areShadesClosed(ShadeID shades) const;
+    void setShades(ShadeID shades);
+    void setShades(uint8_t shades);
 
 private:
     Servo topServo;
     Servo leftServo;
     Servo rightServo;
+    uint8_t shadeState;
 };
+
+// Enable bitwise operators for our enum class so the code looks clean
+inline ServoController::ShadeID operator|(ServoController::ShadeID a, ServoController::ShadeID b) {
+    return static_cast<ServoController::ShadeID>(static_cast<uint8_t>(a) | static_cast<uint8_t>(b));
+}
+// Allows bitwise AND between two ShadeID enums, returning an integer (or bool check)
+inline uint8_t operator&(ServoController::ShadeID a, ServoController::ShadeID b) {
+    return static_cast<uint8_t>(a) & static_cast<uint8_t>(b);
+}
+// Allows bitwise AND between a uint8_t and a ShadeID enum, returning an integer (or bool check)
+inline uint8_t operator&(uint8_t a, ServoController::ShadeID b) {
+    return a & static_cast<uint8_t>(b);
+}
