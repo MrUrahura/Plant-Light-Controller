fbaea22d9899144f523891c99e9a1c17c652f534
Alec Volodarskiy <volodarskiyalec@gmail.com>
2026-07-06 21:02:03 -0700

    Made the entire LightController
    -Made it plan the DLI target for the hour using WeatherForecast's DLI results
    -Made it use a cost-weight algorithm to hit that DLI target for the hour till the photoperiod ends using the current PPFD, the plant's DLI and PPFD preferences, how far along we are in progress, etc.
    -Made it give the result of its algorithm to the ServoController to update the shade state

--- lib/LightController/LightController.cpp
+++ lib/LightController/LightController.cpp
@@ -1,11 +1,89 @@
 #include <Arduino.h>
+#include <cmath>
 #include "LightController.h"
 
-// Enum to represent the state of the light control system
-enum class ControlState {
-    MAXIMIZE_LIGHT,
-    OPTIMIZE_LIGHT,
-    LIMIT_LIGHT,
-    PHOTOPERIOD_COMPLETE
-};
-ControlState currentControlState = ControlState::MAXIMIZE_LIGHT;
+LightController::LightController(const Plant& plant, const LightSensor& sensor, ServoController& shades, const DLITracker& dliTracker, const WeatherForecast& weather, const TimeManager& timeManager)
+    : plant(plant), sensor(sensor), shades(shades), dliTracker(dliTracker), weather(weather), timeManager(timeManager)
+{
+
+}
+
+void LightController::optimizeState() {
+    shades.setShades(findOptimumState());
+}
+
+void LightController::updatePlannedHourlyDLI() {
+    int startHour = timeManager.getHour();
+    int endHour = timeManager.getPhotoperiodEndTime();
+    if (startHour < 0 || endHour > 24 || startHour >= endHour) return;
+
+    std::array<double, 24> hourlyDLI = weather.getHourlyDLI();
+    double physicalDLILeft = weather.getRemainingDLI();
+    if (physicalDLILeft <= 0.0) {
+        Serial.println("LightController: No physical solar DLI remaining for the day.");
+        return;
+    }
+
+    double plantNeededDLI = max(0.0, plant.getTargetDLI() - dliTracker.getCurrentDLI());
+    double targetDLIAllocation = min(physicalDLILeft, plantNeededDLI);
+    
+    currentPlannedDLI = (targetDLIAllocation * (hourlyDLI[startHour] / physicalDLILeft));
+}
+
+uint8_t LightController::findOptimumState() {
+    uint8_t numStates = static_cast<uint8_t>(ServoController::ShadeID::ALL);
+    if(!timeManager.withinPhotoperiod()) {
+        Serial.println("System Alert: Outside active photoperiod. Holding blinds in dark mode until morning.");
+        return numStates;
+        // After this, stop running the algorithm and send a message to the user to put their plants in darkness.
+        // Then, wait until the next time we hit timeManager.startHour.
+    }
+
+    updatePlannedHourlyDLI();
+
+    double minCost = 0.0;
+    uint8_t minState = 0;
+    for(uint8_t i{0}; i < numStates; ++i){
+        shades.setShades(i);
+        delay(50);
+
+        double cost = 0.0;
+
+        int hour, minute, second;
+        timeManager.getCurrentTime(hour, minute, second);
+        int timeInHour = (minute * 60) + second;
+        double planError = (currentPlannedDLI > 0.0) ? (dliTracker.getCurrentDLI() / currentPlannedDLI) - (timeInHour / 3600.0) : 0.0;
+        if(abs(planError) < 0.03) planError = 0;
+        double planWeight = 10 + 50 * abs(planError);
+
+        double currentPPFDLevel = sensor.readPPFDLevel();
+        double targetPPFD = plant.getTargetPPFD();
+        double ppfdError = currentPPFDLevel - targetPPFD;
+        double remainingDLI = weather.getRemainingDLI();
+
+        if((planError > 0 && ppfdError > 0)
+        || (planError < 0 && ppfdError < 0)){
+            cost += planWeight * ppfdError * ppfdError;
+        }
+
+        cost += ppfdError * ppfdError;
+
+        
+        if((targetPPFD < 20)
+        || (targetPPFD < 50 && currentPPFDLevel > 1.2 * targetPPFD)
+        || (targetPPFD < 200 && currentPPFDLevel > 1.5 * targetPPFD)
+        || (targetPPFD < 600 && currentPPFDLevel > 2 * targetPPFD)
+        || (currentPPFDLevel > 2.5 * targetPPFD)){
+            cost += 10000;
+        }
+
+        if(i == 0){
+            minCost = cost;
+        } else if(cost < minCost) {
+            minCost = cost;
+            minState = i;
+        }
+    }
+
+    return minState;
+}
--- lib/LightController/LightController.h
+++ lib/LightController/LightController.h
@@ -1,1 +1,25 @@
 #pragma once
+#include "Plant.h"
+#include "LightSensor.h"
+#include "ServoController.h"
+#include "WeatherForecast.h"
+#include "TimeManager.h"
+#include "DLITracker.h"
+
+class LightController {
+public:
+    LightController(const Plant& plant, const LightSensor& sensor, ServoController& shades, const DLITracker& dliTracker, const WeatherForecast& weather, const TimeManager& timeManager);
+    void optimizeState();
+
+private:
+    const Plant& plant;
+    const LightSensor& sensor;
+    ServoController& shades;
+    const DLITracker& dliTracker;
+    const WeatherForecast& weather;
+    const TimeManager& timeManager;
+    double currentPlannedDLI;
+
+    void updatePlannedHourlyDLI();
+    uint8_t findOptimumState();
+};
