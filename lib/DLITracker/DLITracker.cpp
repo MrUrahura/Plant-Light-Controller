9e523a8a6307f37abd6e6b493f398c9a5deff4dd
Alec Volodarskiy <volodarskiyalec@gmail.com>
2026-07-11 21:36:27 -0700

    Improved DLITracker
    -Made it track when the updates happen so it can accurately gauge the time periods
    -Made it save progress at certain checkpoint times using Preferences so it can remember most of the progress in case of a shut-off

--- lib/DLITracker/DLITracker.cpp
+++ lib/DLITracker/DLITracker.cpp
@@ -1,20 +1,57 @@
 #include <Arduino.h>
 #include "DLITracker.h"
 
-DLITracker::DLITracker(const LightSensor& sensor)
-    : sensor(sensor)
+DLITracker::DLITracker(const LightSensor& sensor, const TimeManager& timeManager)
+    : sensor(sensor), timeManager(timeManager)
 {
-    reset();
+
+}
+
+void DLITracker::loadDLI() {
+    prefs.begin("DLITracker", true);
+    currentDLI = prefs.getDouble("currentDLI", 0.0);
+    lastCheckpointTime = static_cast<time_t>(prefs.getULong64("lastCheckpointTime", 0));
+    prefs.end();
+
+    lastUpdateTime = timeManager.getUnixTime();
 }
 
 void DLITracker::reset() {
-    // Reset DLI at the start of the day
     currentDLI = 0.0;
+
+    time_t now = timeManager.getUnixTime();
+
+    lastUpdateTime = now;
+    lastCheckpointTime = now;
+
+    saveDLI();
 }
 
 void DLITracker::update() {
-    // Update DLI based on the current PPFD reading
-    currentDLI += (sensor.readPPFDLevel() * SAMPLE_PERIOD) / 1000000.0f; // Convert PPFD to DLI (mol/m²/day)
+    time_t now = timeManager.getUnixTime();
+
+    double elapsedSeconds = difftime(now, lastUpdateTime);
+
+    if (elapsedSeconds <= 0) return;
+
+    // Convert PPFD to DLI (mol/m²/day)
+    currentDLI += sensor.readPPFDLevel() * elapsedSeconds / 1000000.0;
+    
+    lastUpdateTime = now;
+
+    // Save every 30 minutes
+    if (difftime(now, lastCheckpointTime) >= 1800)
+    {
+        saveDLI();
+        lastCheckpointTime = now;
+    }
+}
+
+void DLITracker::saveDLI() {
+    prefs.begin("DLITracker", false);
+    currentDLI = prefs.putDouble("currentDLI", currentDLI);
+    prefs.putULong64("lastCheckpointTime", static_cast<uint64_t>(lastCheckpointTime));
+    prefs.end();
 }
 
 double DLITracker::getCurrentDLI() const {
--- lib/DLITracker/DLITracker.h
+++ lib/DLITracker/DLITracker.h
@@ -1,15 +1,23 @@
 #pragma once
+#include <Preferences.h>
 #include "LightSensor.h"
+#include "TimeManager.h"
 
 class DLITracker {
 public:
-    DLITracker(const LightSensor& sensor);    
+    DLITracker(const LightSensor& sensor, const TimeManager& timeManager);
+    void loadDLI();
     void reset();
     void update();
     double getCurrentDLI() const;
 
 private:
+    void saveDLI();
+
     double currentDLI;
     const LightSensor& sensor;
-    const float SAMPLE_PERIOD = 60.0f; // in seconds
+    const TimeManager& timeManager;
+    time_t lastUpdateTime;
+    time_t lastCheckpointTime;
+    Preferences prefs;
 };
